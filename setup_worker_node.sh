03bb1c622441db704b3e42873088331bf50f4856
Benjamin Watkins <watkins.ben@gmail.com>
2025-03-23 14:58:41 +0000

    fix: Add prereq config for br_netfilter to worker script

--- setup_worker_node.sh
+++ setup_worker_node.sh
@@ -315,6 +315,26 @@ else
   apt-get install -qqy kubelet=$k8sVersion kubeadm=$k8sVersion kubectl=$k8sVersion
 fi
 
+# Configuring Prerequisite https://v1-29.docs.kubernetes.io/docs/setup/production-environment/container-runtimes/
+
+echo -e "\033[32mConfiguring Prerequisites\033[0m"
+
+cat <<EOF | tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+
+modprobe overlay
+modprobe br_netfilter
+
+cat <<EOF | tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF
+
+sysctl --system
+
 # Join Cluster
 
 echo -e "\033[32mJoining Kubernetes Cluster\033[0m"
